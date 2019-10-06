dd9a19c346b957b80743a4604a131b32dec81a9a
leefu <2228429150@qq.com>
2019-10-06 16:11:03 +0800

    购物车飞入动画。详情页

--- src/js/es5/particulars.js
+++ src/js/es5/particulars.js
@@ -2,7 +2,9 @@
 
 // 启动放大镜插件
 // 初始化
-$("#exzoom").exzoom(); // 鼠标移入动画
+$("#exzoom").exzoom(); // 初始化cookie
+
+init(); // 鼠标移入动画
 
 $('.shop_sign').mouseenter(function () {
   $(this).css({
@@ -89,4 +91,110 @@ $list.each(function () {
     $(this).addClass('active').siblings().removeClass('active');
     $('.tab_content').find('.tab_detail').eq($(this).index()).addClass('show').siblings().removeClass('show');
   });
-});
+}); // 点击出现购物车
+
+var quick_count = 0;
+$(".click_buy").click(function () {
+  quick_count++;
+  console.log(quick_count);
+
+  if (quick_count % 2 === 1) {
+    $('.right_quick_buy').css({
+      visibility: 'visible'
+    });
+  } else {
+    $('.right_quick_buy').css({
+      visibility: 'hidden'
+    });
+  }
+}); // 购物车
+// 获取加入购物车按钮
+
+$('.Buy_Btn2').click(function () {
+  var SPname = $('.CH').text();
+  console.log(SPname);
+  var SPprice = $('.text_price').text();
+  console.log(SPprice);
+  var SPsrc = $('.SPsrc').attr('src');
+  console.log(SPsrc);
+  var SPid = $('.SPid').text();
+  console.log(SPid);
+  /*	carts
+         {
+             "SPid" : {
+                 ... : ...,
+             }
+         } */
+
+  var cookieStr = $.cookie('carts') ? $.cookie('carts') : '';
+  var cookieObj = cookieStrToObj(cookieStr);
+
+  if (SPid in cookieObj) {
+    cookieObj[SPid].num++;
+  } else {
+    cookieObj[SPid] = {
+      "name": SPname,
+      "price": SPprice,
+      "src": SPsrc,
+      "num": 1
+    };
+  } // 创建cookie
+
+
+  $.cookie('carts', JSON.stringify(cookieObj), {
+    expires: 7,
+    path: '/'
+  }); // 飞入动画
+
+  var $img = $('.SPsrc').clone().css({
+    width: '100px',
+    height: '100px'
+  });
+  $img.fly({
+    start: {
+      left: event.pageX,
+      //开始位置（必填）#fly元素会被设置成position: fixed 
+      top: event.pageY //开始位置（必填） 鼠标点击位置
+
+    },
+    end: {
+      left: $('.cart_num').offset().left + 70,
+      //结束位置（必填） 购物车位置
+      top: $('.cart_num').offset().top,
+      //结束位置（必填） 
+      width: 0,
+      //结束时宽度 
+      height: 0 //结束时高度 
+
+    },
+    onEnd: function onEnd() {
+      //动画结束的时候回调 
+      var num = parseInt($('.cart_num').text());
+      $('.cart_num').text("".concat(num + 1));
+      $('.public_buy_num').text("".concat(num + 1));
+      $img.remove();
+    }
+  });
+}); // cookie页面初始化
+
+function init() {
+  var SPid = $('.SPid').text();
+  var cookieStr = $.cookie('carts') ? $.cookie('carts') : '';
+  var cookieObj = cookieStrToObj(cookieStr);
+  var sum = 0;
+
+  for (SPid in cookieObj) {
+    sum += cookieObj[SPid].num;
+  }
+
+  $('.cart_num').text("".concat(sum));
+} // 转换cookie
+
+
+function cookieStrToObj(str) {
+  if (!str) {
+    return {};
+  }
+
+  return JSON.parse(str);
+}
--- src/js/es6/particulars.js
+++ src/js/es6/particulars.js
@@ -2,6 +2,8 @@
 // 初始化
 $("#exzoom").exzoom();
 
+// 初始化cookie
+init();
 
 // 鼠标移入动画
 $('.shop_sign').mouseenter(function () { 
@@ -84,6 +86,106 @@ $list.each(function () {
     })
 })
 
+
+// 点击出现购物车
+let quick_count = 0;
+$(".click_buy").click(function () { 
+    quick_count++;
+    console.log(quick_count);
+    if (quick_count % 2 === 1) {
+        $('.right_quick_buy').css({ visibility:'visible' });
+    } else { 
+     
+        $('.right_quick_buy').css({ visibility: 'hidden'});
+
+    }
+
+})
+
+// 购物车
+// 获取加入购物车按钮
+$('.Buy_Btn2').click(function () {
+    let SPname = $('.CH').text();
+    console.log(SPname);
+    let SPprice = $('.text_price').text();
+    console.log(SPprice);
+    let SPsrc = $('.SPsrc').attr('src');
+    console.log(SPsrc);
+    let SPid = $('.SPid').text();
+    console.log(SPid);
+    /*	carts
+           {
+               "SPid" : {
+                   ... : ...,
+               }
+           } */
+    let cookieStr = $.cookie('carts') ? $.cookie('carts') : '';
+    let cookieObj = cookieStrToObj(cookieStr);
+    if (SPid in cookieObj) { 
+        cookieObj[SPid].num++;
+    } else {
+        cookieObj[SPid] = {
+            "name": SPname,
+            "price": SPprice,
+            "src": SPsrc,
+            "num" : 1
+        }
+    }
+    // 创建cookie
+    $.cookie('carts', JSON.stringify(cookieObj), {
+        expires : 7,
+        path : '/'
+    })
+
+    // 飞入动画
+    let $img = $('.SPsrc').clone().css({
+        width: '100px',
+        height: '100px'
+    });
+    $img.fly({
+        start: {
+        left: event.pageX, //开始位置（必填）#fly元素会被设置成position: fixed 
+        top: event.pageY //开始位置（必填） 鼠标点击位置
+    },
+    end: {
+        left: $('.cart_num').offset().left+70, //结束位置（必填） 购物车位置
+        top: $('.cart_num').offset().top, //结束位置（必填） 
+        width: 0, //结束时宽度 
+        height: 0 //结束时高度 
+        },
+    
+    onEnd: function() { //动画结束的时候回调 
+        let num = parseInt($('.cart_num').text());
+        $('.cart_num').text(`${num + 1}`);
+        $('.public_buy_num').text(`${num + 1}`);
+ 
+    $img.remove();
+
+    }
+});
+    
+});
+
+// cookie页面初始化
+function init() { 
+    let SPid = $('.SPid').text();
+    let cookieStr = $.cookie('carts') ? $.cookie('carts') : '';
+    let cookieObj = cookieStrToObj(cookieStr);
+    let sum = 0;
+    for (SPid in cookieObj) { 
+        sum += cookieObj[SPid].num;
+    }
+    $('.cart_num').text(`${sum}`);
+
+}
+
+// 转换cookie
+function cookieStrToObj(str) { 
+    if (!str) { 
+        return {};
+    }
+    return JSON.parse(str);
+}
 
 
 
